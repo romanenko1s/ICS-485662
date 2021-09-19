37cfc7dd599dbc37b769285da32c5d9594d9c22e
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-09-19 19:27:35 +0300

    Create universitycheck.py

---
+++ ЛР2/universitycheck.py
@@ -0,0 +1,10 @@
+print("З якого ти факультету")
+userinputraw = str(input())
+userinput = userinputraw.lower()
+
+if userinput == "фіт":
+    while True:
+        print("ФІТ СИЛА  😎💪")
+
+else:
+    print("Прямуй звідси поки я тобі систему не видалив")
