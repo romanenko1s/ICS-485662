b7c0b5d688a0b758e35c8c30229ba0b83b82dfaf
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-09-19 22:16:31 +0300

    Update liveoutside.py

--- ЛР2/liveoutside.py
+++ ЛР2/liveoutside.py
@@ -1,14 +1,14 @@
-def func():
-    x = 0
-    while x < 1000:
-        yield x
-        x+=7
-    print("Я воскрес, дякую")
-
 while True:
     userinput = str(input("Я "))
 
     if userinput == "на чілі":
+        def func():
+            x = 0
+            while x < 1000:
+                yield x
+                x+=7
+            print("Я воскрес, дякую")
+
         for x in func():
             print(x)
 
