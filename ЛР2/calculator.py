d59772e1c4955a8c3b5727397d9d4c0b699c76a7
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-09-20 08:13:31 +0300

    Create calculator.py

---
+++ ЛР2/calculator.py
@@ -0,0 +1,12 @@
+while True:
+    input1 = int(input())
+    input2 = str(input())
+    input3 = int(input())
+    if input2 == "+":
+        print(input1 + input3)
+    if input2 == "-":
+        print(input1 - input3)
+    if input2 == "*":
+        print(input1 * input3)
+    if input2 == "/":
+        print(int(input1 / input3))
