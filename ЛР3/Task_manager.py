1aa11ec0c3d3bced7fc37ea140f72cf9cb45fb9b
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-09-20 09:40:09 +0300

    Update Task_manager.py

--- ЛР3/Task_manager.py
+++ ЛР3/Task_manager.py
@@ -6,7 +6,11 @@ help1 = 'Hello, this is an interactive task manager.\nYou can add, view and dele
 help2 = 'Here is a command list:\n1."Add task" - adds task to your task list\n2."My tasks" - prints all your tasks into the command line\n3."Clear list" - deletes all your tasks\n4."Help" - get a help menu and all additional info\n5."Commands" - gets only list of commands'
 help3 = 'Commands are not case sensetive, you can use capitalised and normal size letters.\nAll changes are automaticaly saved.'
 
+def dec1():
+    print("-------------------")
+
 print(help1)
+dec1()
 
 while True:
     # Here code takes an raw input as a string, and converts it in lowercase
@@ -27,8 +31,9 @@ while True:
         # Here we add new tasks to data file
 
         if userinput == "add task":
+            dec1()
             print("What task is it?")
-
+            
             newtaskinput = (str(input())+"\n")
             
             file_tasklist = open("tasklistdata", "a+")
@@ -36,11 +41,13 @@ while True:
             file_tasklist.close()
 
             print("Task added")
+            dec1()
 
         # Here we print tasks 
 
         if userinput == "my tasks":
             try:
+                dec1()
                 file_tasklist = open("tasklistdata", "r")
                 file_tasklistreadmode = file_tasklist.readlines()
 
@@ -63,6 +70,9 @@ while True:
             except:
                 print("Error ocured during printing from data file\nOr maybe, you just have no tasks")
 
+            finally:
+                dec1()
+
         #Here we clear the data file
 
         if userinput == "clear list":
@@ -81,11 +91,17 @@ while True:
             else:
                 print("Action canceled")
 
+            dec1()
+            print("")
+            dec1()
+
         if userinput == "help":
             print(help1+"\n\n"+help2+"\n\n"+help3)
+            dec1()
 
         if userinput == "commands":
             print(help2)
+            dec1()
 
     except:
         print("Error ocured during opening a data file")
