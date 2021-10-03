37427bd6a1df17b4f506687bbb02559166caa99a
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-10-03 13:52:23 +0300

    Update now_v.1.py

--- ЛР4/now_v.1.py
+++ ЛР4/now_v.1.py
@@ -1,4 +1,5 @@
 import pandas as pd
 
-dataFile = pd.read_excel(r"/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/ЛР4/curencyData.xls")
+dataFile1 = pd.read_excel(r"/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/ЛР4/curencyData.xls")
+dataFile = pd.DataFrame(dataFile1, index=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
 print(dataFile)
