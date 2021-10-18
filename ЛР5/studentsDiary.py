94473ac67a88104f0b97670931cab604233f8d0a
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-10-18 10:35:05 +0300

    Update studentsDiary.py

--- ЛР5/studentsDiary.py
+++ ЛР5/studentsDiary.py
@@ -1,5 +1,33 @@
-file1 = open("studentListData.txt", "r")
-file2 = file1.readlines()
+while True:
 
-for line in file2:
-    print("#  "+line)
+    userInput = str(input())
+
+    if userInput == "0":
+        break
+
+    if userInput == "1":
+        file1 = open("studentListData.txt", "r")
+        file2 = file1.readlines()
+
+        for line in file2:
+            print("#  "+line)
+
+        print("List printed")
+
+    if userInput == "2":
+        print("Enter data")
+
+        newData = (str(input())+"\n")
+
+        file1 = open("studentListData.txt", "a+")
+        file1.write(newData)
+        file1.close()
+
+        print("Data added")
+
+    if userInput == "3":
+        print("What row to print?")
+        row1 = int(input())
+
+        line = open("studentListData.txt").readlines()[row1]
+        print("#  "+line)
