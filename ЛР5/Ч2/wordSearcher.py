7495e939cad1ce994727072ff78ee44c1c838d1c
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-10-31 11:50:27 +0200

    Create wordSearcher.py

---
+++ ЛР5/Ч2/wordSearcher.py
@@ -0,0 +1,22 @@
+words = open('textForWordSearcher.txt').read().lower().split()
+
+uniques = []
+for word in words:
+  if word not in uniques:
+    uniques.append(word)
+
+counts = []
+for unique in uniques:
+  count = 0
+  for word in words:
+    if word == unique:
+      count += 1
+  counts.append((count, unique))
+
+counts.sort()
+counts.reverse()
+#print(counts)
+
+for i in range(min(10, len(counts))):
+  count, word = counts[i]
+  print('%s %d' % (word, count))
