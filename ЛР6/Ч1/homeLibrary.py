428783b94ee1ca5bfc4713d0b980f1e9b9ddad0c
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-02 14:00:32 +0200

    Update homeLibrary.py

---
+++ ЛР6/Ч1/homeLibrary.py
@@ -0,0 +1,7 @@
+class Book():
+
+    def __init__(self, name, author, genre, publicationYear):
+        self.name = name
+        self.author = author
+        self.genre = genre
+        self.publicationYear = publicationYear
