c3d11429440919c1d1b957862a7d73f40e68453a
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-14 14:26:30 +0200

    Update siteAnalizer.py

--- ЛР6/Ч2/siteAnalizer.py
+++ ЛР6/Ч2/siteAnalizer.py
@@ -1,18 +1,34 @@
 import requests
 
+print("Site analizer welcomes you ")
+
 while True:
 
-    print("Enter an url")
+    print("1. Check if some site is existing\n2. Print the cite\n0. Exit")
     userInput = str(input())
 
-    try:
-        r = requests.get(userInput)
+    if userInput == "0":
+        break
 
-        if r.ok == True:
-            print("Found a site with entered url")
+    elif userInput == "1":
+        
+        print("Enter an url")
+        userInput1 = str(input())
 
-        else:
-            print("Can`t find a site with entered url (404)")
+        try:
+            r = requests.get(userInput1)
 
-    except:
-        print("Invalid Input")
+            if r.ok == True:
+                print("Found a site with entered url")
+
+            else:
+                print("Can`t find a site with entered url (404)")
+
+        except:
+            print("Invalid input of url")
+
+    elif userInput == "2":
+        pass
+
+    else:
+        print("Invalid input")
