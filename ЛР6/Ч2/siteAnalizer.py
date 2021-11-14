57d5ed2826c5199fa03e61e78b50abc02587223d
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-14 15:37:02 +0200

    Update siteAnalizer.py

--- ЛР6/Ч2/siteAnalizer.py
+++ ЛР6/Ч2/siteAnalizer.py
@@ -1,10 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 print("Site analizer welcomes you ")
 
 while True:
 
-    print("1. Check if some site is existing\n2. Print the cite\n0. Exit")
+    print("1. Check if some site is existing\n2. Print the cite\n3. Analize a cite content\n0. Exit")
     userInput = str(input())
 
 
@@ -39,7 +40,26 @@ while True:
             r = requests.get(userInput2)
 
             if r.ok == True:
-                print(r.text)
+                page = BeautifulSoup(r.text, 'html.parser')
+                print(page)
+
+            else:
+                print("Can`t reach a site with entered url (404)")
+
+        except:
+            print("Invalid input of url")
+
+
+    elif userInput == "3":
+
+        print("Enter an url")
+        userInput2 = str(input())
+
+        try:
+            r = requests.get(userInput2)
+
+            if r.ok == True:
+                pass ##########################
 
             else:
                 print("Can`t reach a site with entered url (404)")
