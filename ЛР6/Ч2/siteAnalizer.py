89224214bc6906508d6f57f4caed3d0457cc1370
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-14 14:39:43 +0200

    Update siteAnalizer.py

--- ЛР6/Ч2/siteAnalizer.py
+++ ЛР6/Ч2/siteAnalizer.py
@@ -7,11 +7,13 @@ while True:
     print("1. Check if some site is existing\n2. Print the cite\n0. Exit")
     userInput = str(input())
 
+
     if userInput == "0":
         break
 
+
     elif userInput == "1":
-        
+
         print("Enter an url")
         userInput1 = str(input())
 
@@ -19,16 +21,32 @@ while True:
             r = requests.get(userInput1)
 
             if r.ok == True:
-                print("Found a site with entered url")
+                print("Site with entered url exists")
 
             else:
-                print("Can`t find a site with entered url (404)")
+                print("Can`t reach a site with entered url (404)")
 
         except:
             print("Invalid input of url")
 
+
     elif userInput == "2":
-        pass
+
+        print("Enter an url")
+        userInput2 = str(input())
+
+        try:
+            r = requests.get(userInput2)
+
+            if r.ok == True:
+                print(r.text)
+
+            else:
+                print("Can`t reach a site with entered url (404)")
+
+        except:
+            print("Invalid input of url")
+
 
     else:
         print("Invalid input")
