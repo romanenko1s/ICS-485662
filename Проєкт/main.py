a77ec3fe5224526c7ee75374f0cb6ed135c3aa90
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-23 21:58:34 +0200

    Update main.py

--- Проєкт/main.py
+++ Проєкт/main.py
@@ -3,12 +3,8 @@ import matplotlib as ml
 import matplotlib.pyplot as plt
 #import tkinter as tk
 
-excelPath = "/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/ЛР7/curencyData.xls"
-csvPath = "/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/ЛР7/curencyData.csv"
+excelPath = "/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/Проєкт/curencyData.xls"
 
 dataFile1 = pd.read_excel(excelPath)
-dataFile = pd.DataFrame(dataFile1, index=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18])
-for data in dataFile:
-    print(data)
-dataFile.plot(x = "")
-plt.show()
+dataFile = pd.DataFrame(dataFile1, index=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
+print(dataFile)
