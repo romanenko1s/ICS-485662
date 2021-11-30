fb454a196c667574984689d46527b469cb395567
Illya <I.Romanenko_FIT_7_21_B_d@knute.edu.ua>
2021-11-30 15:24:25 +0200

    Update main.py

--- Проєкт/main.py
+++ Проєкт/main.py
@@ -5,6 +5,9 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 import xlrd
+from menu import Ui_nowMenu
+from window1 import Ui_window1
+from window2 import Ui_window2
 
 ### WORKING WITH DATA BASES ###
 
@@ -14,141 +17,96 @@ excelPath3 = "/home/user/GitHub/romanenko1s/romanenko1s/ICS-485662/Проєкт/
 dataExcelFile1 = pd.read_excel(excelPath1)
 dataExcelFile12 = pd.DataFrame(dataExcelFile1, index=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
 dataFile = str(dataExcelFile12)
-
-### MINOR WINDOWS ###
-
-
-
-### WORKING WITH THE MAIN WINDOW ###
-
-class Ui_MainWindow(object):
-
-    def setupUi(self, MainWindow):
-
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(603, 427)
-        MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
-
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.title = QtWidgets.QLabel(self.centralwidget)
-        self.title.setGeometry(QtCore.QRect(0, 0, 600, 51))
-        font = QtGui.QFont()
-        font.setFamily("Ubuntu")
-        font.setPointSize(25)
-        font.setBold(True)
-        font.setWeight(75)
-        self.title.setFont(font)
-        self.title.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.title.setStyleSheet("background-color: rgb(245, 194, 17);")
-        self.title.setAlignment(QtCore.Qt.AlignCenter)
-        self.title.setObjectName("title")
-
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(225, 100, 150, 40))
-        self.pushButton.setStyleSheet("background-color: rgb(246, 245, 244);\n""color: rgb(0, 0, 0);")
-        self.pushButton.setObjectName("pushButton")
-        #self.pushButton.clicked.connect(self.button1Action)
-
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(225, 150, 150, 40))
-        self.pushButton_2.setStyleSheet("background-color: rgb(246, 245, 244);\n""color: rgb(0, 0, 0);")
-        self.pushButton_2.setObjectName("pushButton_2")
-        #self.pushButton_2.clicked.connect(self.button2Action)
-
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(225, 200, 150, 40))
-        self.pushButton_3.setStyleSheet("background-color: rgb(246, 245, 244);\n""color: rgb(0, 0, 0);")
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_3.clicked.connect(self.button3Action)
-
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(225, 250, 150, 40))
-        self.pushButton_4.setStyleSheet("background-color: rgb(246, 245, 244);\n""color: rgb(0, 0, 0);")
-        self.pushButton_4.setObjectName("pushButton_4")
-        #self.pushButton_4.clicked.connect(self.button4Action)
-
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setGeometry(QtCore.QRect(225, 300, 150, 40))
-        self.pushButton_5.setStyleSheet("background-color: rgb(246, 245, 244);\n""color: rgb(0, 0, 0);")
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.pushButton_5.clicked.connect(self.close)
-
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 603, 22))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Зараз"))
-        self.title.setText(_translate("MainWindow", "Зараз"))
-        self.pushButton.setText(_translate("MainWindow", "Курс валют"))
-        self.pushButton_2.setText(_translate("MainWindow", "Номера валют"))
-        self.pushButton_3.setText(_translate("MainWindow", "Графік валют"))
-        self.pushButton_4.setText(_translate("MainWindow", "Вивести в ексель"))
-        self.pushButton_5.setText(_translate("MainWindow", "Вихід"))
-
-    def button1Action(self):
-        pass
-
-    def button2Action(self):
-        pass
-
-    def button3Action(self):
-        dataExcelFile3 = pd.read_excel(excelPath3)
-
-        # Here we read each row in the excel sheet
-        y1 = dataExcelFile3.iloc[0]
-        y2 = dataExcelFile3.iloc[1]
-        y3 = dataExcelFile3.iloc[2]
-        y4 = dataExcelFile3.iloc[3]
-        y5 = dataExcelFile3.iloc[4]
-        y6 = dataExcelFile3.iloc[5]
-
-        # Here we add each row to graph
-        plt.plot(y1, label = u'103')
-        plt.plot(y2, label = u'104')
-        plt.plot(y3, label = u'105')
-        plt.plot(y4, label = u'106')
-        plt.plot(y5, label = u'109')
-        plt.plot(y6, label = u'111')
-       
-        # The decoration section
-        plt.title("Курс Валют")
-        plt.ylabel("Курс відносно гривні")
-        plt.xlabel("Дата")
-        plt.grid()
-        plt.legend()
-
-        plt.show()
-    
-    def button4Action(self):
-        fname = QFileDialog.getSaveFileName(self)[0]
-        try:
-            f = open(fname, "w")
-            f.write(dataFile)
-            f.close()
-        except:
-            pass
-
-    def close(self):
-        QApplication.closeAllWindows()
-
 ### STARTING APP ###
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())  
+app = QtWidgets.QApplication(sys.argv)
+
+nowMenu = QtWidgets.QDialog()
+ui = Ui_nowMenu()
+ui.setupUi(nowMenu)
+nowMenu.show()
+
+### DECLARING FUNCTIONS ###
+
+def openFirstWindow():
+    global window1
+    window1 = QtWidgets.QDialog()
+    ui = Ui_window1()
+    ui.setupUi(window1)
+    nowMenu.close()
+    window1.show()
+
+    def returnToMain1():
+        window1.close()
+        nowMenu.show()
+
+    ui.window1PushButton_1.clicked.connect(returnToMain1)
+
+def openSecondWindow():
+    global window2
+    window2 = QtWidgets.QDialog()
+    ui = Ui_window2()
+    ui.setupUi(window2)
+    nowMenu.close()
+    window2.show()
+
+    def returnToMain2():
+        window2.close()
+        nowMenu.show()
+
+    ui.window2PushButton_1.clicked.connect(returnToMain2)
+
+
+def drawGraph():
+    dataExcelFile3 = pd.read_excel(excelPath3)
+
+    # Here we read each row in the excel sheet
+    y1 = dataExcelFile3.iloc[0]
+    y2 = dataExcelFile3.iloc[1]
+    y3 = dataExcelFile3.iloc[2]
+    y4 = dataExcelFile3.iloc[3]
+    y5 = dataExcelFile3.iloc[4]
+    y6 = dataExcelFile3.iloc[5]
+
+    # Here we add each row to graph
+    plt.plot(y1, label = u'103')
+    plt.plot(y2, label = u'104')
+    plt.plot(y3, label = u'105')
+    plt.plot(y4, label = u'106')
+    plt.plot(y5, label = u'109')
+    plt.plot(y6, label = u'111')
+    
+    # The decoration section
+    plt.title("Курс Валют")
+    plt.ylabel("Курс відносно гривні")
+    plt.xlabel("Дата")
+    plt.grid()
+    plt.legend()
+
+    plt.show()
+
+def saveExcelFile():
+    fname = QFileDialog.getSaveFileName()[0]
+    try:
+        f = open(fname, "w")
+        f.write(dataFile)
+        f.close()
+    except:
+        pass
+
+def close():
+    QApplication.closeAllWindows()
+
+### CONECTING FUNCTIONS TO APP ###
+
+ui.menuPushButton_1.clicked.connect(openFirstWindow)
+ui.menuPushButton_2.clicked.connect(openSecondWindow)
+ui.menuPushButton_3.clicked.connect(drawGraph)
+ui.menuPushButton_5.clicked.connect(close)
+
+### EXIT THE APP ###
+
+sys.exit(app.exec_())
+
+### DECLARING FUNCTIONS ###
+
